8973e6b5a344eee272586eb20435153ec955a6a8
marianmagur <33460464+marianmagur@users.noreply.github.com>
2019-01-16 21:09:04 +0200

    Update and rename calculation.py to test_calculation.py

--- homeworks/magurmarian/hw01/test_calculation.py
+++ homeworks/magurmarian/hw01/test_calculation.py
@@ -1,8 +1,9 @@
+"""module for testing add and devide functions"""
 import argparse
 import sys
 
 from pyats.aetest import Testcase, test, main
-from somefunc import add, divide
+from calculation import add, divide
 
 parameters = {
     'num1': 3,
@@ -11,17 +12,18 @@ parameters = {
 
 
 class SmokeTest(Testcase):
-
+    """class for smoke testing add and devide functions"""
     @test
-    def test_add(self, steps, num1, num2):
-        with steps.start('Result bigger then 0') as step:
-            if add(num1, num2) > 0:
-                assert add(num1, num2) > 0
-            else:
-                self.skipped('Result is less then 0)')
+    def test_add(self, num1, num2):
+        """method for testing add function"""
+        if add(num1, num2) >= 0:
+            assert add(num1, num2) >= 0
+        else:
+            self.skipped('Result is less then 0)')
 
     @test
     def test_devide(self, steps, num1, num2):
+        """method for testing devide function"""
         with steps.start('Divide') as step:
             try:
                 assert divide(num1, num2) > 0
@@ -33,14 +35,16 @@ class SmokeTest(Testcase):
 
 
 def inputs():
+    """parsing user arguments here"""
     parser = argparse.ArgumentParser(description="standalone parser")
     parser.add_argument('-num1', dest='num1', type=int, required=False, default=parameters['num1'])
-    parser.add_argument('-num2', dest='num2', type=int, required=False, default=parameters['num1'])
+    parser.add_argument('-num2', dest='num2', type=int, required=False, default=parameters['num2'])
 
     args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
     return args
 
 
 if __name__ == '__main__':
+
     INPUT_DATA = inputs()
     main(num1=INPUT_DATA.num1, num2=INPUT_DATA.num2)
