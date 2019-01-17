35fbb4ab6d86096fd0e7b028722dfd358440c617
Serge [Tempos] Kutwicki <Serge.Kutwicki@gmail.com>
2019-01-17 20:03:47 +0200

    Updates following the reviews.
    Removed redundant code
    Removed default "parameters" and substituted it with parser's default values.
    Switched input_data variable name to lovercase.

--- homeworks/kutwicki_serge/HW01/test_calculation.py
+++ homeworks/kutwicki_serge/HW01/test_calculation.py
@@ -1,16 +1,10 @@
 """Homework_01 pyATS. Test for calculation.py"""
 # pylint: disable=C0103 - ignore constant name "parameters"
 
+from pyats.aetest import Testcase, test, main
 import argparse
 import sys
-
 from calculation import add, divide
-from pyats.aetest import Testcase, test, main
-
-parameters = {
-    'num1': 3,
-    'num2': 0
-}
 
 
 class SmokeTest(Testcase):
@@ -20,36 +14,30 @@ class SmokeTest(Testcase):
     def test_add(self, steps, num1, num2):
         """Test addition functionality."""
         with steps.start(f'Addition of {num1} and {num2}'):
-            summ = add(num1, num2)
-            if summ < 0:
+            if add(num1, num2) < 0:
                 self.skipped(f'Test was skipped because result of adding {num1} and {num2} is less then 0')
-            else:
-                assert summ == num1 + num2
 
     @test
     def test_divide(self, steps, num1, num2):
         """Test division functionality."""
-        with steps.start(f'Division of {num1} by {num2}') as step:
+        with steps.start(f'Division of {num1} by {num2}'):
             try:
-                div_result = divide(num1, num2)
-                if div_result < 0:
+                if divide(num1, num2) < 0:
                     self.skipped(f'Test was skipped because result of division {num1} by {num2} is less then 0')
-                else:
-                    assert div_result == num1 / num2
             except ZeroDivisionError:
-                step.passx(f'Division of {num1} by {num2}', from_exception=ZeroDivisionError('ZeroDivisionError'))
+                self.passx(f'Division of {num1} by {num2}', from_exception=ZeroDivisionError('ZeroDivisionError'))
 
 
 def inputs():
     """Arguments parser setup."""
     parser = argparse.ArgumentParser(description="standalone parser")
-    parser.add_argument('-num1', dest='num1', type=int, required=False, default=parameters['num1'])
-    parser.add_argument('-num2', dest='num2', type=int, required=False, default=parameters['num2'])
+    parser.add_argument('-num1', dest='num1', type=int, required=False, default=3)
+    parser.add_argument('-num2', dest='num2', type=int, required=False, default=0)
 
     args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
     return args
 
 
 if __name__ == '__main__':
-    INPUT_DATA = inputs()
-    main(num1=INPUT_DATA.num1, num2=INPUT_DATA.num2)
+    input_data = inputs()
+    main(num1=input_data.num1, num2=input_data.num2)
