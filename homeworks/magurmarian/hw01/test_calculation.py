5b065307cf6c0bc41910484cac5783c1cfaa3d12
marianmagur <33460464+marianmagur@users.noreply.github.com>
2019-01-18 13:14:06 +0200

    Update test_calculation.py

--- homeworks/magurmarian/hw01/test_calculation.py
+++ homeworks/magurmarian/hw01/test_calculation.py
@@ -16,9 +16,7 @@ class SmokeTest(Testcase):
     @test
     def test_add(self, num1, num2):
         """method for testing add function"""
-        if add(num1, num2) >= 0:
-            assert add(num1, num2) >= 0
-        else:
+        if add(num1, num2) < 0:
             self.skipped('Result is less then 0)')
 
     @test
@@ -31,20 +29,17 @@ class SmokeTest(Testcase):
                 step.passx(f'num1:{num1} ,num2:{num2}',
                            from_exception=ZeroDivisionError('ZeroDivisionError'))
             except AssertionError:
-                self.skipped('Result is less then 0)')
-
-
-def inputs():
-    """parsing user arguments here"""
-    parser = argparse.ArgumentParser(description="standalone parser")
-    parser.add_argument('-num1', dest='num1', type=int, required=False, default=parameters['num1'])
-    parser.add_argument('-num2', dest='num2', type=int, required=False, default=parameters['num2'])
-
-    args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
-    return args
+                step.skipped('Result is less then 0)')
 
 
 if __name__ == '__main__':
+    parameters = {
+        'num1': 3,
+        'num2': 0
+    }
+    parser = argparse.ArgumentParser(description="standalone parser")
+    parser.add_argument('-num1', dest='num1', type=int, required=False)
+    parser.add_argument('-num2', dest='num2', type=int, required=False)
 
-    INPUT_DATA = inputs()
-    main(num1=INPUT_DATA.num1, num2=INPUT_DATA.num2)
+    args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
+    main(num1=args.num1, num2=args.num2)
