8ab6c28e4984496138b053c62f61230cc016ae70
marianmagur <33460464+marianmagur@users.noreply.github.com>
2019-01-21 15:37:45 +0200

    Update test_calculation.py

--- homeworks/magurmarian/hw01/test_calculation.py
+++ homeworks/magurmarian/hw01/test_calculation.py
@@ -8,11 +8,16 @@ from calculation import add, divide
 
 class SmokeTest(Testcase):
     """class for smoke testing add and devide functions"""
+    parameters = {
+        'num1': -3,
+        'num2': 1
+    }
+
     @test
     def test_add(self, num1, num2):
         """method for testing add function"""
         if add(num1, num2) < 0:
-            self.skipped('Result is less then 0)')
+            self.skipped('Result of add is less then 0')
 
     @test
     def test_devide(self, steps, num1, num2):
@@ -22,19 +27,17 @@ class SmokeTest(Testcase):
                 assert divide(num1, num2) > 0
             except ZeroDivisionError:
                 step.passx(f'num1:{num1} ,num2:{num2}',
-                           from_exception=ZeroDivisionError('ZeroDivisionError'))
+                           from_exception=ZeroDivisionError('Zero Division Error. No way!'))
             except AssertionError:
-                step.skipped('Result is less then 0)')
+                step.skipped('Result of divide is less then 0')
 
 
 if __name__ == '__main__':
-    parameters = {
-        'num1': 3,
-        'num2': 0
-    }
+
     parser = argparse.ArgumentParser(description="standalone parser")
     parser.add_argument('-num1', dest='num1', type=int, required=False)
     parser.add_argument('-num2', dest='num2', type=int, required=False)
 
     args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
     main(num1=args.num1, num2=args.num2)
+
