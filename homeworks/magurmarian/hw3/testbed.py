f58edc047d9a641dc779b55c23fbf4c15a787f07
marianmagur <marianmagur@gmail.com>
2019-01-28 09:15:39 -0800

    implement my homework3

--- homeworks/magurmarian/hw3/testbed.py
+++ homeworks/magurmarian/hw3/testbed.py
@@ -10,19 +10,9 @@ class Smoke(Testcase):
                 source='scp://192.168.195.143/home/adminaccount/some.txt',
                 destination='/home/marian/github/pyatsTraining/homeworks/magurmarian/hw3')
 
-
-class Smoke1(Testcase):
     @test
     def copy_to_remote_pc(self, tb):
         with FileUtils(testbed=tb) as futils:
             futils.copyfile(
                 source='/home/marian/github/pyatsTraining/homeworks/magurmarian/hw3/some.txt',
                 destination='scp://192.168.195.143/home/adminaccount')
-
-
-class Smoke2(Testcase):
-    @test
-    def check_existence_file(self, tb):
-        with FileUtils(testbed=tb) as futils:
-            futils.checkfile(
-                target='sftp://192.168.195.143/home/adminaccount/some.txt')
